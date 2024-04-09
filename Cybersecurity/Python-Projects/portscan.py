ca92cec61bbb5494c1194d8b2b9c70a0082cb5ac
mk76git <150850347+mk76git@users.noreply.github.com>
2024-04-09 14:46:57 -0400

    Add files via upload

---
+++ Cybersecurity/Python-Projects/portscan.py
@@ -0,0 +1,19 @@
+import sys
+import socket
+import pyfiglet
+
+
+ascii_banner = pyfiglet.figlet_format("TryHackMe \n Python 4 Pentesters \nPort $
+print(ascii_banner)
+
+
+ip = '10.10.111.199' 
+open_ports =[] 
+
+ports = range(1, 65535)
+
+
+def probe_port(ip, port, result = 1): 
+  try: 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    sock.settimeout(0.5) 
